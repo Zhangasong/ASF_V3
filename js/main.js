304c560cf230a3baa0ce74eb8eb5cb80ad082d9a
Zhangasong <105703052@mail2.nccu.tw>
2019-05-09 00:39:47 +0800

    see mobile

---
+++ js/main.js
@@ -0,0 +1,87 @@
+(function(){
+  // Vertical Timeline - by CodyHouse.co
+	function VerticalTimeline( element ) {
+		this.element = element;
+		this.blocks = this.element.getElementsByClassName("cd-timeline__block");
+		this.images = this.element.getElementsByClassName("cd-timeline__img");
+		this.contents = this.element.getElementsByClassName("cd-timeline__content");
+		this.offset = 0.8;
+		this.hideBlocks();
+	};
+
+	VerticalTimeline.prototype.hideBlocks = function() {
+		if ( !"classList" in document.documentElement ) {
+			return; // no animation on older browsers
+		}
+		//hide timeline blocks which are outside the viewport
+		var self = this;
+		for( var i = 0; i < this.blocks.length; i++) {
+			(function(i){
+				if( self.blocks[i].getBoundingClientRect().top > window.innerHeight*self.offset ) {
+					self.images[i].classList.add("cd-timeline__img--hidden"); 
+					self.contents[i].classList.add("cd-timeline__content--hidden"); 
+				}
+			})(i);
+		}
+	};
+
+	VerticalTimeline.prototype.showBlocks = function() {
+		if ( ! "classList" in document.documentElement ) {
+			return;
+		}
+		var self = this;
+		for( var i = 0; i < this.blocks.length; i++) {
+			(function(i){
+				if( self.contents[i].classList.contains("cd-timeline__content--hidden") && self.blocks[i].getBoundingClientRect().top <= window.innerHeight*self.offset ) {
+					// add bounce-in animation
+					self.images[i].classList.add("cd-timeline__img--bounce-in");
+					self.contents[i].classList.add("cd-timeline__content--bounce-in");
+					self.images[i].classList.remove("cd-timeline__img--hidden");
+					self.contents[i].classList.remove("cd-timeline__content--hidden");
+				}
+			})(i);
+		}
+	};
+
+	var verticalTimelines = document.getElementsByClassName("js-cd-timeline"),
+		verticalTimelinesArray = [],
+		scrolling = false;
+	if( verticalTimelines.length > 0 ) {
+		for( var i = 0; i < verticalTimelines.length; i++) {
+			(function(i){
+				verticalTimelinesArray.push(new VerticalTimeline(verticalTimelines[i]));
+			})(i);
+		}
+
+		//show timeline blocks on scrolling
+		window.addEventListener("scroll", function(event) {
+			if( !scrolling ) {
+				scrolling = true;
+				(!window.requestAnimationFrame) ? setTimeout(checkTimelineScroll, 250) : window.requestAnimationFrame(checkTimelineScroll);
+			}
+		});
+	}
+
+	function checkTimelineScroll() {
+		verticalTimelinesArray.forEach(function(timeline){
+			timeline.showBlocks();
+		});
+		scrolling = false;
+	};
+})();
+
+
+ var wow = new WOW(
+      {
+        boxClass:     'wow',      // 要套用WOW.js縮需要的動畫class(預設是wow)
+        //animateClass: 'animated', // 要"動起來"的動畫(預設是animated, 因此如果你有其他動畫library要使用也可以在這裡調整)
+        offset:       0,          // 距離顯示多遠開始顯示動畫 (預設是0, 因此捲動到顯示時才出現動畫)
+        mobile:       true,       // 手機上是否要套用動畫 (預設是true)
+        live:         true,       // 非同步產生的內容是否也要套用 (預設是true, 非常適合搭配SPA)
+        callback:     function(box) {
+          draw()
+        },
+        scrollContainer: null // 可以設定成只套用在某個container中捲動才呈現, 不設定就是整個視窗
+      }
+    );
+    wow.init();
